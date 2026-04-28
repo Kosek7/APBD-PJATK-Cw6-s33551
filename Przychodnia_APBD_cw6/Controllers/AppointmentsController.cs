5a72bde9f7cc4f3734a9196a85f087b12dcf47d5
Dawid-Piotrowski-s33551 <s33551@pjwstk.edu.pl>
2026-04-29 01:54:31 +0200

    create first GET endpoint

---
+++ Przychodnia_APBD_cw6/Controllers/AppointmentsController.cs
@@ -0,0 +1,71 @@
+﻿using Przychodnia_APBD_cw6.DTOs;
+
+namespace Przychodnia_APBD_cw6.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class AppointmentsController : ControllerBase
+{
+    private readonly string _connectionString;
+    
+    public AppointmentsController(IConfiguration configuration)
+        {
+        _connectionString = configuration.GetConnectionString("DefaultConnection")!;
+        }
+    
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAppointments(
+        [FromQuery] string? status,
+        [FromQuery] string? patientLastName)
+        {
+            var result = new List<AppointmentListDto>();
+
+            await using var connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync();
+
+            await using var command = new SqlCommand(@"
+                SELECT
+                    a.IdAppointment,
+                    a.AppointmentDate,
+                    a.Status,
+                    a.Reason,
+                    p.FirstName + ' ' + p.LastName AS PatientFullName,
+                    p.Email AS PatientEmail
+                FROM dbo.Appointment a
+                JOIN dbo.Patient p ON p.IdPatient = a.IdPatient
+                WHERE (@Status IS NULL OR a.Status = @Status)
+                AND (@PatientLastName IS NULL OR p.LastName = @PatientLastName)
+                ORDER BY a.AppointmentDate;
+            ",  connection);
+            
+            command.Parameters.Add("@Status", SqlDbType.NVarChar, 30)
+                .Value = (object?)status ?? DBNull.Value;
+            
+            command.Parameters.Add("@PatientLastName", SqlDbType.NVarChar, 80)
+                .Value = (object?)patientLastName ?? DBNull.Value;
+
+            await using var reader = await command.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                result.Add(new AppointmentListDto
+                {
+                    IdAppointment = reader.GetInt32(0),
+                    AppointmentDate = reader.GetDateTime(1),
+                    Status = reader.GetString(2),
+                    Reason = reader.GetString(3),
+                    PatientFullName = reader.GetString(4),
+                    PatientEmail = reader.GetString(5)
+                });
+            }
+            
+            return Ok(result);
+        }
+}
